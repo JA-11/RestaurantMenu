f9968f3c0debc1ea87999f646360853112271f83
Jordan <jordan_anthony11@aol.com>
2022-08-17 19:59:39 -0500

    Add project files.

---
+++ RestaurantMenu/Menu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RestaurantMenu
+{
+    public class Menu
+    {
+        public List<MenuItem> Goodfood { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public Menu(List<MenuItem> goodfood, DateTime dateTime)
+        {
+            Goodfood = goodfood;
+            DateTime = dateTime;
+        }
+
+    }
+}
